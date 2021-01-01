6713fe23a3f0ff54c0601193def957df2e973124
Carlos Garcia <carlosaicrag@gmail.com>
2021-01-01 13:33:41 -0800

    add views to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,43 @@ class UsersController < ApplicationController
     end
   end
 
+  def new 
+    @user = User.new
+    render :new
+  end
+
   def create
     debugger
-    user = User.new(params.require(:user).permit(:username,:email))
+    @user = User.new(params.require(:user).permit(:username,:email))
 
+    if @user.save
+      render json: @user
+    else
+      render :new
+    end
+  end
+
+  def edit 
+
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    if user.nil?
+      render json: "user not found"
+    elsif user.update(user_params)
+      render json: user
+    else
+      render json: "we couldn't update user"
+    end
+  end
+
+  def delete
+    user = User.find_by(id: params[:id])
+
+    user.destroy
+
+    render json: user
   end
 
   private 
